820cceaed6fc5dbc88016da8e50bc0b821af7fc7
Rene Quiambao <therealrq@gmail.com>
2022-08-07 13:14:45 -0400

    added coding challenge

---
+++ cc-2022-08-07/solution.js
@@ -0,0 +1,24 @@
+// You need to write a function, that returns the first non-repeated character in the given string.
+
+// If all the characters are unique, return the first character of the string.
+// If there is no unique character, return null in JS or Java, None in Python, '\0' in C.
+
+// You can assume, that the input string has always non-zero length.
+
+// Examples
+// "test"   returns "e"
+// "teeter" returns "r"
+// "trend"  returns "t" (all the characters are unique)
+// "aabbcc" returns null (all the characters are repeated)
+
+function firstNonRepeated(s) {
+    let string = s.split('')
+
+    for (let i = 0; i < string.length; i++) {
+        if (string.lastIndexOf(string[i]) === string.indexOf(string[i])) {
+            return string[i]
+        }
+    }
+
+    return null
+}
