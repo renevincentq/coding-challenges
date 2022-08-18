8c40b19c3c8777e9320cf1196a0a44ab17312ff5
Rene Quiambao <therealrq@gmail.com>
2022-08-18 13:05:15 -0400

    added coding challenge

---
+++ cc-2022-08-18/solution.js
@@ -0,0 +1,55 @@
+// 35. Search Insert Position
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums contains distinct values sorted in ascending order.
+// -104 <= target <= 104
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let left = 0,
+        right = nums.length - 1,
+        mid,
+        index = nums.length
+
+    while (left <= right) {
+        mid = left + Math.floor((right - left) / 2)
+        if (nums[mid] === target) {
+            return mid
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1
+        }
+        else {
+            index = mid
+            right = mid - 1
+        }
+    }
+
+    return index
+};
