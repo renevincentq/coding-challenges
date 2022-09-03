17f8e2d2e5cb38182d4688e94baa27c0718627c2
Rene Quiambao <therealrq@gmail.com>
2022-09-03 13:46:38 -0400

    added coding challenge

---
+++ cc-2022-09-03/solution.js
@@ -0,0 +1,27 @@
+// Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 3, print out “Fizz”; when it’s divisible by 5, print out “Buzz”; when it’s divisible by both 3 and 5, print out “Fizz Buzz”.
+
+//num with no funny biz
+//log each num or the appr word
+//n = 5 -> 1, 2, fizz, 4, buzz
+//n = 9 -> 1, 2, fizz, 4, buzz, fizz, 7, 8, fizz
+//n = 15 ->  1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz
+
+function fizzBuzz(num) {
+    for (let i = 1; 1 <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('Fizz Buzz')
+        }
+        else if (i % 3 === 0) {
+            console.log('Fizz')
+        }
+        else if (i % 5 === 0) {
+            console.log('Buzz')
+        }
+        else {
+            console.log(i)
+        }
+    }
+}
+
+console.log(fizzBuzz(5))
+console.log(fizzBuzz(15))
