1d56098b66ef50fa01da5b3882bfd7463ab0a50a
Rene Quiambao <therealrq@gmail.com>
2022-10-02 15:09:50 -0400

    added coding challenge

---
+++ cc-2022-10-02/solution.js
@@ -0,0 +1,18 @@
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+// sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
+// sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
+function sortArray(arr) {
+    const oddSort = arr.filter(x => x % 2 !== 0).sort((a, b) => a - b)
+
+    return arr.map(x => x % 2 !== 0 ? oddSort.shift() : x)
+}
+
+console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))
+console.log(sortArray([5, 8, 6, 3, 4]))
+console.log(sortArray([7, 1]))
