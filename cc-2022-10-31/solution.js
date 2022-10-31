4b5c082f30b2cab0bed3247860cdef2e3a9ae6e1
Rene Quiambao <therealrq@gmail.com>
2022-10-31 16:02:46 -0400

    added coding challenge

---
+++ cc-2022-10-31/solution.js
@@ -0,0 +1,22 @@
+// Given a string, return a new string that has transformed based on the input:
+
+// Change case of every character, ie. lower case to upper case, upper case to lower case.
+// Reverse the order of words from the input.
+// Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+// For example:
+
+// "Example Input" ==> "iNPUT eXAMPLE"
+// You may assume the input only contain English alphabet and spaces.
+
+function transformStr(str) {
+    return str.split(' ')
+        .reverse()
+        .join(' ')
+        .split('')
+        .map(x => x === x.toUpperCase() ? x.toLowerCase() : x.toUpperCase()).join('')
+}
+
+console.log(transformStr("Example Input"))
+console.log(transformStr('testing testtt'))
+console.log(transformStr('hi im BOB hello'))
