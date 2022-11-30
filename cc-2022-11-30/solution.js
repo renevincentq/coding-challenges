0c604ffba0241174ad291a851690c97a5520c8c9
Rene Quiambao <therealrq@gmail.com>
2022-11-30 18:25:13 -0500

    added coding challenge

---
+++ cc-2022-11-30/solution.js
@@ -0,0 +1,20 @@
+// In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+function repeats(arr) {
+    let num = 0,
+        nums = {}
+
+    for (const number of arr) {
+        nums[number] = nums[number] + 1 || 1
+    }
+
+    for (const number in nums) {
+        if (nums[number] === 1) {
+            num += Number(number)
+        }
+    }
+
+    return num
+};
