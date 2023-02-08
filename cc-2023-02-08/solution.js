64af302f0189a6cc4317211c43f0bf702b556298
Rene Quiambao <therealrq@gmail.com>
2023-02-08 18:41:24 -0500

    added coding challenge

---
+++ cc-2023-02-08/solution.js
@@ -0,0 +1,39 @@
+// 169. Majority Element
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    let count = maj = 0
+
+    for (const num of nums) {
+        if (count === 0) maj = num
+        if (num !== maj) count--
+        else count++
+    }
+
+    return maj
+};
