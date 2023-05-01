8a05f8d488aa6d231bd894946bdf15dde2250acd
Rene Quiambao <therealrq@gmail.com>
2023-05-01 13:43:52 -0400

    added coding challenge

---
+++ cc-2023-05-01/solution.js
@@ -0,0 +1,15 @@
+// Given an array of strings of the same letter type. Return a new array, which will differ in that the length of each element is equal to the average length of the elements of the previous array.
+
+// A few examples:
+
+// ['u', 'y'] =>  ['u', 'y'] // average length is 1
+// ['aa', 'bbb', 'cccc'] => ['aaa', 'bbb', 'ccc'] // average length is 3
+// ['aa', 'bb', 'ddd', 'eee'] => ['aaa', 'bbb', 'ddd', 'eee'] // average length is 2.5 round up to 3
+// If the average length is not an integer, use Math.round().
+// The input array's length > 1
+
+
+function averageLength(strings) { 
+    let num = Math.round(strings.reduce((a, b) => a + b.length, 0)/strings.length)
+    return strings.map(x => x.charAt(0).repeat(num))
+}
