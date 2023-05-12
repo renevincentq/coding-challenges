26d5e614ca613db70782ca9ac9782cce807ef9da
Rene Quiambao <therealrq@gmail.com>
2023-05-12 12:09:54 -0400

    added coding challenge

---
+++ cc-2023-05-12/solution.js
@@ -0,0 +1,30 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+function appearTwice(num) {
+    let nums = {}
+    for (let i = 0; i < num.length; i++) {
+        if (nums[num[i]]) {
+            return true
+        }
+        else {
+            nums[num[i]] = true
+        }
+    }
+    return false
+}
+
+console.log(appearTwice([1, 1, 1, 1, 1, 1]))
+console.log(appearTwice([1, 2, 3, 4, 5, 6]))
