03ecafe8c988919bb50a4112fcfde4ea25004db0
Rene Quiambao <therealrq@gmail.com>
2023-08-25 17:04:18 -0400

    added coding challenge

---
+++ cc-2023-08-25/solution.js
@@ -0,0 +1,54 @@
+// 1200. Minimum Absolute Difference
+
+// Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
+
+// Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+
+// a, b are from arr
+// a < b
+// b - a equals to the minimum absolute difference of any two elements in arr
+
+
+// Example 1:
+
+// Input: arr = [4,2,1,3]
+// Output: [[1,2],[2,3],[3,4]]
+// Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
+// Example 2:
+
+// Input: arr = [1,3,6,10,15]
+// Output: [[1,3]]
+// Example 3:
+
+// Input: arr = [3,8,-10,23,19,-4,-14,27]
+// Output: [[-14,-10],[19,23],[23,27]]
+
+
+// Constraints:
+
+// 2 <= arr.length <= 105
+// -106 <= arr[i] <= 106
+
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function (arr) {
+    let sort = [...arr].sort((a, b) => a - b)
+    let min = Infinity
+    let res = []
+
+    for (let i = 0; i < sort.length - 1; i++) {
+        const diff = sort[i + 1] - sort[i]
+
+        if (diff < min) {
+            min = diff
+            res = [[sort[i], sort[i + 1]]]
+        }
+        else if (diff === min) {
+            res.push([sort[i], sort[i + 1]])
+        }
+    }
+
+    return res
+};
